865277307fd938c25c2de825ffc83f255a308f43
Sameer <akita.sameer1@gmail.com>
2025-12-02 18:05:50 +0900

    arrays

---
+++ array/array.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nums [4]int
+
+	// nums[0] = 1
+
+	// fmt.Println(nums)
+
+	// fmt.Println(len(nums))
+
+	// var vals [4]bool
+	// vals[2] = true
+	// fmt.Println(vals)
+
+	// var names [3]string
+	// names[1] = "sameer"
+	// fmt.Println(names)
+
+	// nums := [3]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	nums := [2][2]int{{1, 2}, {3, 4}}
+	fmt.Println(nums)
+}
