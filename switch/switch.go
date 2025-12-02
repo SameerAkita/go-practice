8215e3078642220198c704839eec780330d12366
Sameer <akita.sameer1@gmail.com>
2025-12-02 17:12:11 +0900

    time switch

--- switch/switch.go
+++ switch/switch.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	i := 5
+	// i := 5
 
-	switch i {
-	case 1:
-		fmt.Println("one")
-	case 2:
-		fmt.Println("two")
+	// switch i {
+	// case 1:
+	// 	fmt.Println("one")
+	// case 2:
+	// 	fmt.Println("two")
+	// default:
+	// 	fmt.Println("other")
+	// }
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("its a weekend")
 	default:
-		fmt.Println("other")
+		fmt.Println("its a weekday")
 	}
 }
