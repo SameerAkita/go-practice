1f5fc8d98657c751f325bd3209e6dc1c476ccf4f
Sameer <akita.sameer1@gmail.com>
2025-12-14 18:38:44 +0900

    range

---
+++ range/range.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// nums := []int {6, 7, 8}
+
+
+	// for i, num := range nums {
+	// 	fmt.Println(num, i)
+		
+	// }
+	// m := map[string]string{"fname": "john", "lname": "doe"}
+
+	// for k, v := range m {
+	// 	fmt.Println(k, v)
+	// }
+
+	for i, c := range "golang" {
+		fmt.Println(i, string(c))
+	}
+}
