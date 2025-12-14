44ba32e1954382744d3e71f0c3175de90112a2cd
Sameer <akita.sameer1@gmail.com>
2025-12-14 18:08:25 +0900

    slices

---
+++ slices/slices.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// nums := []int{}
+	// nums = append(nums, 1)
+	// nums = append(nums, 2)
+	// nums[0] = 3
+	// fmt.Println(nums)
+	// fmt.Println(cap(nums))
+	// fmt.Println(len(nums))
+	// var nums = make([]int, 0, 5)
+	// nums = append(nums, 2)
+	// var nums2 = make([]int, len(nums))
+
+	// copy(nums2, nums)
+
+	// fmt.Println(nums)
+	// fmt.Println(nums2)
+
+	// var nums = []int {1, 2, 3}
+
+	// fmt.Println(nums[1:])
+
+	// var n`ums1 = []int{1, 2}
+	// var nums2 = []int{1, 2}
+
+	// fmt.Println(slices.Equal(nums1, nums2))
+	
+	var nums = [][]int{{1, 2, 3}, {4, 5, 6}}
+	fmt.Println(nums)
+}
