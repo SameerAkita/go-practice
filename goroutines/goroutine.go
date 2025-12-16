79c3a2b6dcf166554b0c6bc7be58941d7580d6af
Sameer <akita.sameer1@gmail.com>
2025-12-17 03:31:03 +0900

    goroutines

---
+++ goroutines/goroutine.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(id int) {
+	fmt.Println("doing task", id)
+}
+
+func main() {
+	for i:=0; i <= 10; i++ {
+		go func(i int) {
+			fmt.Println(i)
+		} (i)
+	}
+
+	time.Sleep(time.Second * 2)
+}
